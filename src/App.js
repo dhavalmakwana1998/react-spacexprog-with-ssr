1b5e1bd0b4d849079050f39ed361ebb51ab20d83
Dhaval Makawana <dhavalmakawana191@gmail.com>
2021-08-24 14:22:18 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="./images/logo512.png" className="App-logo" alt="logo" />
+        <h4>Welcome to spacex program</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
